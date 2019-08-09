bf4a4b9847a7e81028a1bc49f5147d97a7664b98
BPitts8019 <bradleyj.pitts@gmail.com>
2019-08-08 19:24:05 -0700

    Messing with a mouseover event

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
-// Your code goes here
+(() => {
+   //get page elements
+   const images = document.querySelectorAll(".img-content");
+
+   //setup event listeners
+   images.forEach(img => {
+      img.addEventListener("mouseenter", event => {
+         console.log(event.currentTarget);
+      });
+
+      img.addEventListener("mouseleave", event => {
+         console.log(event.currentTarget);
+         // event.currentTarget.parent.style.backgroundColor = "";
+      });
+   });
+
+})();
