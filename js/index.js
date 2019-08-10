2a399e7e721e55e7ce4cc93fd895f197daff0e2a
BPitts8019 <bradleyj.pitts@gmail.com>
2019-08-09 17:22:01 -0700

    Added mouseEnter and mouseLeave events to images

--- js/index.js
+++ js/index.js
@@ -1,17 +1,25 @@
 (() => {
+   //event handlers
+   // const toggleOverlay = event => {
+   //    const children = Array.from(event.currentTarget.childNodes);
+   //    const overlays = children.filter(child => {
+   //       let isGood = (child.classList)? child.classList.contains("overlay") : false;
+   //       return isGood;
+   //    });
+   
+   //    overlays[0].style.opacity = (overlays[0].style.opacity)? "" : "1";
+   // };
+   const togglePicBlur = event => {
+      event.currentTarget.style.filter = (event.currentTarget.style.filter)? "" : "blur(5px)";
+   };
+
    //get page elements
-   const images = document.querySelectorAll(".img-content");
+   const images = document.querySelectorAll("img");
 
    //setup event listeners
    images.forEach(img => {
-      img.addEventListener("mouseenter", event => {
-         console.log(event.currentTarget);
-      });
-
-      img.addEventListener("mouseleave", event => {
-         console.log(event.currentTarget);
-         // event.currentTarget.parent.style.backgroundColor = "";
-      });
+      img.addEventListener("mouseenter", togglePicBlur); 
+      img.addEventListener("mouseleave", togglePicBlur);
    });
 
 })();
